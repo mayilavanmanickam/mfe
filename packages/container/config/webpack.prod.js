7cd8fce1a7188c68453e3c9653ce3ac2c11499da
Mayilavan Manickam <38646345+mayilavanmanickam@users.noreply.github.com>
2022-05-19 16:01:12 +0530

    added comment to trigger build

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -1,3 +1,4 @@
+// prod
 const { merge } = require("webpack-merge");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common");
