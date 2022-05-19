510b18cda4a7f89c99fa8ce1df1ca945a145af55
MayilavanManickam <mayilavan.manickam@target.com>
2022-05-19 16:08:52 +0530

    removed the comment

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -1,4 +1,3 @@
-// prod
 const { merge } = require("webpack-merge");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common");
